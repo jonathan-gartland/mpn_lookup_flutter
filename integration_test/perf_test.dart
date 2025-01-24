1e78602b3587bdd332c8ff071b25948c07b9cefa
jonathan-gartland <jonathan-gartland@gmail.com>
2025-01-24 18:53:34 -0500

    fix: husky, maybe

--- integration_test/perf_test.dart
+++ integration_test/perf_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:testing_app/main.dart';
+import 'package:mpn_lookup_flutter/main.dart';
 
 void main() {
   group('Testing App Performance', () {
@@ -9,7 +9,7 @@ void main() {
     binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
 
     testWidgets('Scrolling test', (tester) async {
-      await tester.pumpWidget(const TestingApp());
+      await tester.pumpWidget(const MyApp());
 
       final listFinder = find.byType(ListView);
 
